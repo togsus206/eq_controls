8f9f576756783b0e2c0ead9eefb03fa129f0189a
Matias <togsus@gmail.com>
2025-02-06 11:42:16 -0300

    Modificacion y adicion en marca samsung

--- index.php
+++ index.php
@@ -73,9 +73,6 @@
             <?php include 'modules_sources/SAMSUNG/SAMSUNG.html'; ?>
 
             
-            <!-- Samsung Grande-->
-            <?php include 'modules_sources/samsung_grande.html'; ?>
-            
             <!-- LG MAGIC -->
             <?php include 'modules_sources/LG_magic/LG_magic.html'; ?>
 
