0c14d706de78bb9c4cf12f0688a9b3b2eb23751c
Matias <togsus@gmail.com>
2025-07-17 11:47:42 -0300

    Se agrego un distintivo para reconocer cuando una comparacion se refiere a 2 modelos no compatibles y aclaracion sobre nueva informacion sobre los controles NOHIS Y SAPCO

--- sub_pages_sources/telefunken.php
+++ sub_pages_sources/telefunken.php
@@ -44,10 +44,10 @@
             <?php include '../modules_sources/1532_vs_721.html'; ?>
 
             <!-- BGH - TELEFUNKEN vs NOBLEX  -->
-            <?php include 'modules_sources/606_vs_680.html'; ?>
+            <?php include '../modules_sources/606_vs_680.html'; ?>
 
             <!-- NOBLEX - SANYO - ADMIRAL VS TELEFUNKEN   -->
-            <?php include 'modules_sources/676_vs_724.html'; ?>
+            <?php include '../modules_sources/676_vs_724.html'; ?>
 
 
         <footer class="text-center mt-8">
